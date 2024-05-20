93b0652272751ca6143fd2409a6b6dac436869ea
Diego <diegodutra2000@hotmail.com>
2024-05-20 19:08:40 -0300

    criando novas migrations

---
+++ database/migrations/create_products.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('customer_id');
+            $table->string('name');
+            $table->string('brand');
+            $table->unsignedInteger('quantity');
+            $table->float('weight');
+            $table->enum('weight_unit', ['g', 'kg', 't']);
+            $table->decimal('price', total: 8, places: 2);
+            $table->text('description')->nullable();
+            $table->dateTime('create');
+            $table->dateTime('update')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
